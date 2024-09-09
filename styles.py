5d4f75eabe77273d57c9b9dc0ea275ce7c419e21
kelle <kellerhydle@gmail.com>
2024-09-08 18:52:51 -0600

    #14 updates to styles.py
    -added support for Pyinstaller _MEIPASS issues

--- styles.py
+++ styles.py
@@ -1,5 +1,8 @@
-with open('mp_software_stylesheets/mac_style.css', 'r') as f:
-    macCSS = f.read()
+import sys, os
 
-with open('mp_software_stylesheets/windows_style.css', 'r') as f:
-    windowsCSS = f.read()
+if getattr(sys, 'frozen', False):
+    macCSS = open(os.path.join(sys._MEIPASS, 'mp_software_stylesheets/mac_style.css'), 'r').read()
+    windowsCSS = open(os.path.join(sys._MEIPASS, 'mp_software_stylesheets/windows_style.css'), 'r').read()
+else:
+    macCSS = open('mp_software_stylesheets/mac_style.css', 'r').read()
+    windowsCSS = open('mp_software_stylesheets/windows_style.css', 'r').read()
