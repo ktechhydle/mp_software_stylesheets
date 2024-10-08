1bedd999029d9c04cee925e1df76d58f23ff3395
kelle <kellerhydle@gmail.com>
2024-10-07 21:11:54 -0600

    #30 file updates
    -optimized styles.py
    -added unified.css for one main stylesheet

--- styles.py
+++ styles.py
@@ -1,8 +1,22 @@
 import sys, os
 
-if getattr(sys, 'frozen', False):
-    macCSS = open(os.path.join(sys._MEIPASS, 'mp_software_stylesheets/mac_style.css'), 'r').read()
-    windowsCSS = open(os.path.join(sys._MEIPASS, 'mp_software_stylesheets/windows_style.css'), 'r').read()
-else:
-    macCSS = open('mp_software_stylesheets/mac_style.css', 'r').read()
-    windowsCSS = open('mp_software_stylesheets/windows_style.css', 'r').read()
+# Determine base path depending on frozen or non-frozen environment
+base_path = os.path.join(sys._MEIPASS, 'mp_software_stylesheets') if getattr(sys, 'frozen',
+                                                                             False) else 'mp_software_stylesheets'
+
+# Load CSS files with error handling
+try:
+    with open(os.path.join(base_path, 'mac_style.css'), 'r') as mac_file:
+        macCSS = mac_file.read()
+
+    with open(os.path.join(base_path, 'windows_style.css'), 'r') as windows_file:
+        windowsCSS = windows_file.read()
+
+    with open(os.path.join(base_path, 'unified.css'), 'r') as unified_file:
+        unifiedCSS = unified_file.read()
+
+except FileNotFoundError as e:
+    print(f"Error: {e}. File not found.")
+except IOError as e:
+    print(f"Error: {e}. An I/O error occurred.")
+
