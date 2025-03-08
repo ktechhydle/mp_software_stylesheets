0c5792dcd46e9f15564b8d086d941adcbacda322
kelle <kellerhydle@gmail.com>
2025-03-07 17:47:04 -0700

    Repo Updates
    -updated mp_software_stylesheets internal repo

--- styles.py
+++ styles.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 
 # Determine base path depending on frozen or non-frozen environment
 base_path = os.path.join(sys._MEIPASS, 'mp_software_stylesheets') if getattr(sys, 'frozen',
@@ -6,20 +7,11 @@ base_path = os.path.join(sys._MEIPASS, 'mp_software_stylesheets') if getattr(sys
 
 # Load CSS files with error handling
 try:
-    with open(os.path.join(base_path, 'mac_style.css'), 'r') as mac_file:
-        macCSS = mac_file.read()
-
-    with open(os.path.join(base_path, 'windows_style.css'), 'r') as windows_file:
-        windowsCSS = windows_file.read()
-
-    with open(os.path.join(base_path, 'unified.css'), 'r') as unified_file:
-        unifiedCSS = unified_file.read()
-
-    with open(os.path.join(base_path, 'blender_style.css'), 'r') as blender_file:
-        blenderCSS = blender_file.read()
+    with open(os.path.join(base_path, 'dot39_style.css'), 'r') as stylesheet_f:
+        dot39CSS = stylesheet_f.read()
 
 except FileNotFoundError as e:
-    print(f"Error: {e}. File not found.")
+    print(f'Error: {e}. File not found.')
 except IOError as e:
-    print(f"Error: {e}. An I/O error occurred.")
+    print(f'Error: {e}. An I/O error occurred.')
 
