1d424144b2ec4e296d779a446b88d3cd6952246b
kelle <kellerhydle@gmail.com>
2025-03-30 12:13:30 -0600

    Stylesheet Updates
    -added dot39_style.css and mprun_style.css
    -updated styles.py

--- styles.py
+++ styles.py
@@ -1,14 +1,14 @@
 import sys
 import os
 
-# Determine base path depending on frozen or non-frozen environment
 base_path = os.path.join(sys._MEIPASS, 'mp_software_stylesheets') if getattr(sys, 'frozen',
                                                                              False) else 'mp_software_stylesheets'
 
-# Load CSS files with error handling
 try:
-    with open(os.path.join(base_path, 'style.css'), 'r') as stylesheet_f:
-        AppCSS = stylesheet_f.read()
+    with open(os.path.join(base_path, 'dot39_style.css'), 'r') as stylesheet_f:
+        DOT39CSS = stylesheet_f.read()
+    with open(os.path.join(base_path, 'mprun_style.css'), 'r') as stylesheet_f:
+        MPRUNCSS = stylesheet_f.read()
 
 except FileNotFoundError as e:
     print(f'Error: {e}. File not found.')
